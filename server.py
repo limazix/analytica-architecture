8130b740c7464c2a6178ba1a0bafd787fb1c3923
Bruno Cardoso <blimacardoso@fb.com>
2021-08-04 19:04:33 -0300

    hello world 2

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 PORT = 8080
 name = os.environ.get('NAME')
 if name == None or len(name) == 0:
-  name = "world"
+  name = "world 2"
 MESSAGE = "Hello, " + name + "!"
 print("Message: '" + MESSAGE + "'")
 
